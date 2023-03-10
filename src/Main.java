9baf8b0be72ceae195f6d37ae7816e6351792126
krek <kraksy8@gmail.com>
2023-03-10 09:08:44 +0100

    meh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import javax.swing.*;
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class Main {
+
+    String day[] = {"SUN" , "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
+    String month[] = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
+
+    static Calendar calender = Calendar.getInstance();
+    static JFrame frame = new JFrame("mTracker");
+
+    static JPanel panel = new JPanel();
+    static JLabel week = new JLabel();
+
+    static int weekCount = calender.get(Calendar.WEEK_OF_MONTH);
+    static TimeZone blank = calender.getTimeZone();
+
+    static int dayOfMonth = calender.get(Calendar.DAY_OF_MONTH);
+    static int dayCount =
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 600);
+
+
+        week.setText(String.valueOf(num));
+        frame.add(week);
+
+
+    }
+
+
+
+    static public void menu () {
+
+
+
     }
 }
